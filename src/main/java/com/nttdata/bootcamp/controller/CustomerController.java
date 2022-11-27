39d2fbe624a684abc1872a6c507880c871c3afe5
Ruben Jonatan Maza Vilchez <rmazavil@emeal.nttdata.com>
2022-11-27 08:27:26 -0500

    update

--- src/main/java/com/nttdata/bootcamp/controller/CustomerController.java
+++ src/main/java/com/nttdata/bootcamp/controller/CustomerController.java
@@ -21,7 +21,7 @@ import reactor.core.publisher.Mono;
 
 //@Slf4j
 @RestController
-@RequestMapping(value = "v1/api/customer")
+@RequestMapping(value = "customer")
 public class CustomerController {
 	
 	//private static Logger LOGGER = Logger.getLogger(CustomerController.class);

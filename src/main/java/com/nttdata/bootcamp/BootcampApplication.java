102a2362edb1c84a0a6bd78d113713227bbcbb06
Ruben Jonatan Maza Vilchez <rmazavil@emeal.nttdata.com>
2022-12-01 20:46:54 -0500

    update

--- src/main/java/com/nttdata/bootcamp/BootcampApplication.java
+++ src/main/java/com/nttdata/bootcamp/BootcampApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 @OpenAPIDefinition(info =
-	@Info(title = "Employee API", version = "1.0", description = "Documentation Employee API v1.0")
+	@Info(title = "Customer API", version = "1.0", description = "Documentation Customer API v1.0")
 )
 public class BootcampApplication {
 
